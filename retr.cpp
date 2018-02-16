14e9b675f63465e5c645cb275d303d8dce322e75
varvara <varvara@DESKTOP-606PFG6>
2018-02-16 15:47:26 +0300

    Add existing file

--- retr.cpp
+++ retr.cpp
@@ -6,6 +6,35 @@
 #include <vector>
 #define inf 1e10
 
+struct color {
+	unsigned int  
+};
+class colorRGB {
+public:
+	unsigned int  R;
+	unsigned G;
+	unsigned B;
+
+	explicit colorRGB() {
+		R = 255;
+		B = 255;
+		G = 255;
+	}
+
+	colorRGB(unsigned const R, unsigned const G, unsigned const B) 
+		: R(R), G(G), B(B) {};
+	
+	~colorRGB() {};
+
+	colorRGB operator *(double const k) {
+		R = (unsigned) k * R;
+		G = (unsigned) k * ;
+		B *= k;
+		if (R < 0) R = 0;
+		else if (R > 255) return 
+	}
+};
+
 class vec3 {
 
 public:
@@ -14,8 +43,10 @@ public:
 	double z;
 
 	explicit vec3() {};
-	vec3(const double x, const double y, const double z) : 
-					x(x), y(y), z(z) {};
+
+	vec3(const double x, const double y, const double z) :
+		x(x), y(y), z(z) {};
+
 	~vec3() {};
 
 	vec3 &operator -(vec3 const &other) {
@@ -25,11 +56,18 @@ public:
 		return *this;
 	}
 
-	double operator *(const vec3 &other) {
+	vec3 &operator +(vec3 const &other) {
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		return *this;
+	}
+
+	double  operator *(const vec3 &other) {
 		return (x * other.x + y * other.y + z * other.z);
 	}
 
-	vec3 operator *(double d) {
+	vec3 &operator *(double d) {
 		x *= d;
 		y *= d;
 		z *= d;
@@ -42,22 +80,80 @@ public:
 
 	vec3 normalize() {
 		double l = this->len();
-		return (*this * (1/l));
+		return (*this * (1 / l));
 	}
-	
+
 };
+
+class light {
+public:
+	double intensity = 0;
+	virtual double computeLighting(vec3 const &Point, vec3 const &Normal) { return 0; };
+};
+
+class ambientLight : public light {
+public:
+	ambientLight(const double &intensity) {
+		this->intensity = intensity;
+	}
+
+	double computeLighting(vec3 const &Point, vec3 const &Normal) { 
+		return intensity; 
+	};
+	~ambientLight() {};
+};
+
+class pointLight : public light {	
+	
+	vec3 position;
+
+public:
+	pointLight(const double &intensity, const vec3 &position) : position(position) {
+		this->intensity = intensity;
+	}
+
+	double computeLighting(vec3 const &Point, vec3 const &Normal) {
+		vec3 L;
+		L = position - Point;
+
+		double Normal_dot_L = L * Normal;
+		if (Normal_dot_L > 0)
+			return intensity * Normal_dot_L / L.len();
+		else return 0;
+	}
+	~pointLight() {};
+};
+
+class directionalLight : public light {
+
+	vec3 direction;
+
+public:
+	directionalLight(const double &intensity, const vec3 &direction) : direction(direction) {
+		this->intensity = intensity;
+	}
+
+	double computeLighting(vec3 const &Point, vec3 const &Normal) {
+		double Normal_dot_L = direction * Normal;
+		if (Normal_dot_L > 0)
+			return intensity * Normal_dot_L / direction.len();
+		else return 0;
+	}
+
+	~directionalLight() {};
+};
+
+
 
 class object {
 public:
 	COLORREF color;
 	vec3 position;
-public:
+
 	virtual bool Intersect(vec3 const &start, vec3 const &direction, double *t) { return false; };
 };
 
 class Sphere: public object {
-
-//class Sphere {
 	
 private:
 	double radius, radius2;
@@ -92,42 +188,75 @@ public:
 	}
 };
 
-COLORREF TraceRay(const vec3 &start, const vec3 &direction, std::vector<object*> &scene) {
+COLORREF TraceRay(const vec3 &start, vec3 &direction, std::vector<object*> &scene, std::vector<light*> &lightning) {
 
-	const Sphere* sphere = NULL;
 	double *t = new double[2];
-	double t_near = inf;
-	double t0 = inf, t1 = inf;
-	COLORREF tempcolor = RGB(255, 255, 255);
+	t[0] = inf, t[1] = inf;
+
+	double closest_t = inf;
+	size_t closest_object = -1;
 
 	for (size_t i = 0; i < scene.size(); i++) {
-		double t0 = inf, t1 = inf;
-		if (scene[i]->Intersect(start, direction, t)) 
-			tempcolor = scene[i]->color;
+
+		if (scene[i]->Intersect(start, direction, t))
+			if (t[0] < closest_t) {
+				closest_object = i;
+				closest_t = t[0];
+			}
+			else if  (t[1] < closest_t) {
+				closest_object = i;
+				closest_t = t[1];
+			}
 	}
-	return tempcolor;
+
+	if (closest_object == -1) return RGB(255, 255, 255);
+
+	vec3 intersectPoint = direction * closest_t + start;
+	vec3 Normal;
+	Normal = intersectPoint - scene[closest_object]->position;
+	Normal.normalize();
+
+	double totalIntensity = 0;
+
+	for (size_t i = 0; i < lightning.size(); i++) {
+		totalIntensity += lightning[i]->computeLighting(intersectPoint, Normal);
+	}
+
+	
+	return scene[closest_object]->color * totalIntensity;
 };
 
 void Draw_Scene(HDC hdc) {
 	
-	vec3 O = vec3(0, 0, 0); 
+	int imageWigth = 640,
+		imageHeight = 640,
+		screenDistanse = 640;
+
+	vec3 O = vec3(0, 0, 0);
+
 	std::vector<object*> scene;
-	//std::vector<Sphere> scene;
+	
+	scene.push_back(&Sphere(vec3(0, imageHeight, 3 * screenDistanse), 640, RGB(255, 0, 0)));
+	scene.push_back(&Sphere(vec3(2 * imageWigth, 0, 4 * screenDistanse), 640, RGB(0, 255, 0)));
+	scene.push_back(&Sphere(vec3(- 2 * imageWigth, 0, 4 * screenDistanse), 640, RGB(0, 0, 255)));
 
-	scene.push_back(&Sphere(vec3(10, 10, 100), 10, RGB(255, 0, 0)));
-	scene.push_back(&Sphere(vec3(30, 30, 100), 30, RGB(0, 255, 0)));
-	scene.push_back(&Sphere(vec3(100, 100, 100), 100, RGB(0, 0, 255)));
+	std::vector<light*> lighting;
 
-	int imaheWigth = 640,
-		imageHeight = 640;
+	lighting.push_back(&ambientLight(0.2));
+	lighting.push_back(&pointLight(0.6, vec3(2 * imageWigth, 2 * imageHeight, 0)));
+	lighting.push_back(&directionalLight(0.2, vec3(imageWigth, 4 * imageHeight, 4 * screenDistanse)));
 
-	for (int i = 0; i < imaheWigth; ++i)
+
+	for (int i = 0; i < imageWigth; ++i)
 	{
 		for (int j = 0; j < imageHeight; ++j)
 		{
-			vec3 D = vec3(i, j, 1000);
+			double x = i - imageWigth / 2;
+			double y = j - imageHeight / 2;
+			vec3 D = vec3(x, y, screenDistanse);
 			D.normalize();
-			SetPixel(hdc, i, j, TraceRay(O, D, scene));
+
+			SetPixel(hdc, i, j, TraceRay(O, D, scene, lighting));
 		}
 	}
 return;
